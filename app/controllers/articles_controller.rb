0e15ad7d20894a46ff4dce8bd865b414fb1e2ff9
Allison <allisonmariebrowne@gmail.com>
2014-12-16 23:09:21 -0500

    form outputs to show.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,26 @@
 class ArticlesController < ApplicationController
-def new
-end
+  def new
+  end
 
-def create
-  @article = Article.new(params[:article])
-  @article.save
-  redirect_to @article
-end
+  def index
+    @articles = Articles.all
+  end
 
+  def create
+    #creates a new article object based on the form
+    @article = Article.new(article_params)
+    #saves the new article object to the database
+    @article.save
+    #redirects to the show action
+    redirect_to @article
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
